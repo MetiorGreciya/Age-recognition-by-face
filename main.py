35719f9e9d1efb0d4e7147cc431e1f19b7974bfe
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-07 23:14:22 +0300

    update

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ df = pd.DataFrame(data)
 print(df)
 
 df.to_csv("people.csv")
+
+img = cv2.imread('images/Matthew_McConaughey.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = cv2.CascadeClassifier('models/face.xml')
+results = faces.detectMultiScale(gray, scaleFactor=1.5, minNeighbors= 2)
+
+for (x, y, w, h) in results:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+
+cv2.imshow("Results", img)
+cv2.waitKey(0)
