759455be0962055186266ec38eabffa22b93c8b8
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-07 21:09:57 +0300

    update

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ from deepface import DeepFace
 data = {
     "Name": [],
     "Age": [],
-    "Gender": []
+    "Gender": [],
+    "Race": []
 }
 
 for file in os.listdir("faces"):
-    result = DeepFace.analyze(cv2.imread(f"faces/{file}"), actions=("age", "gender"))
+    result = DeepFace.analyze(cv2.imread(f"faces/{file}"), actions=("age", "gender", "race"))
     data["Name"].append(file.split(".")[0])
     data["Age"].append(result[0]["age"])
     data["Gender"].append(result[0]["dominant_gender"])
+    data["Race"].append(result[0]["dominant_race"])
 
+df = pd.DataFrame(data)
+print(df)
+
+df.to_csv("people.csv")
