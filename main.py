9f59a603a0bbd28b76df888f9843023adb727692
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-07 20:46:15 +0300

    Импорт библиотек

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import os
 import cv2
+import pandas as pd
 from deepface import DeepFace
+
+data = {
+    "Name": [],
+    "Age": [],
+    "Gender": []
+}
+
+for file in os.listdir("faces"):
+    result = DeepFace.analyze(cv2.imread(f"faces/{file}"), actions=("age", "gender"))
+    data["Name"].append(file.split(".")[0])
