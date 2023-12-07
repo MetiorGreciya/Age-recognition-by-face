0fb17df53e68a21fbd70c8c66bbfaa4abaea86ad
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-08 01:01:46 +0300

    update

--- pyqt.py
+++ pyqt.py
@@ -3,12 +3,30 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import sys
 
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("ARBF")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Тут должно будет что-то написано")
+        self.main_text.move(70, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(120, 150)
+        self.btn.setText("Нажми на меня!")
+        self.btn.adjustSize()
+        self.btn.clicked.connect(self.add_label)
+    def add_label(self):
+        print("Здарова!")
+
 def application():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-
-    window.setWindowTitle("Age recognition by face")
-    window.setGeometry(300, 250, 350, 200)
+    window = Window()
 
     window.show()
     sys.exit(app.exec_())
