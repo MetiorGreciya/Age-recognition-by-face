e647155cab4c03d3dbcb6f758305b277f9d38c0e
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-10 13:08:53 +0300

    update

--- main.py
+++ main.py
@@ -71,14 +71,17 @@ print(df)
 
 df.to_csv("people.csv")
 
-img = cv2.imread('images/Matthew_McConaughey.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = cv2.CascadeClassifier('models/face.xml')
-results = faces.detectMultiScale(gray, scaleFactor=1.5, minNeighbors= 2)
+    def analyzeImage(self):
+        if hasattr(self, 'image_path'):
+            img = cv2.imread(self.image_path)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-for (x, y, w, h) in results:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+            faces = cv2.CascadeClassifier('models/face.xml')
+            results = faces.detectMultiScale(gray, scaleFactor=1.5, minNeighbors= 2)
 
-cv2.imshow("Results", img)
-cv2.waitKey(0)
+            for (x, y, w, h) in results:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+
+            cv2.imshow("Results", img)
+            cv2.waitKey(0)
