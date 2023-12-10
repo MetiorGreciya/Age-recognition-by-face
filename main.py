d111df3148226034cfca7168ec0792dea40662fe
MetiorGreciya <124148535+MetiorGreciya@users.noreply.github.com>
2023-12-10 13:04:35 +0300

    update

--- main.py
+++ main.py
@@ -2,22 +2,69 @@ import os
 import cv2
 import pandas as pd
 from deepface import DeepFace
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QLineEdit
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+class ImageAnalyzerApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Image Analyzer App')
+        self.setGeometry(100, 100, 800, 600)
+
+        self.image_label = QLabel(self)
+        self.image_label.setAlignment(Qt.AlignCenter)
+
+        self.btn_open = QPushButton('Open Image', self)
+        self.btn_open.clicked.connect(self.openImage)
+
+        self.btn_analyze = QPushButton('Analyze Image', self)
+        self.btn_analyze.clicked.connect(self.analyzeImage)
+
+        self.name_input = QLineEdit(self)
+        self.age_input = QLineEdit(self)
+        self.gender_input = QLineEdit(self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        layout.addWidget(self.btn_open)
+        layout.addWidget(self.btn_analyze)
+        layout.addWidget(QLabel('Name:'))
+        layout.addWidget(self.name_input)
+        layout.addWidget(QLabel('Age:'))
+        layout.addWidget(self.age_input)
+        layout.addWidget(QLabel('Gender:'))
+        layout.addWidget(self.gender_input)
+
+        self.setLayout(layout)
+
+    def openImage(self):
+        fname, _ = QFileDialog.getOpenFileName(self, 'Open Image', '', 'Image Files (*.png *.jpg *.bmp)')
+        if fname:
+            pixmap = QPixmap(fname)
+            self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), Qt.KeepAspectRatio))
+            self.image_label.setAlignment(Qt.AlignCenter)
+
+            self.image_path = fname
+
 
 data = {
     "Name": [],
     "Age": [],
     "Gender": [],
-    "Race": []
 }
 
 for file in os.listdir("images"):
     result = DeepFace.analyze(cv2.imread(f"images/{file}"),
-                              actions=("age", "gender", "race"))
+                              actions=("age", "gender"))
 
     data["Name"].append(file.split(".")[0])
     data["Age"].append(result[0]["age"])
     data["Gender"].append(result[0]["dominant_gender"])
-    data["Race"].append(result[0]["dominant_race"])
 
 df = pd.DataFrame(data)
 print(df)
